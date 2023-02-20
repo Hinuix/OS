ebcc7d7d5965ab42b386a404216e194ca2120c42
Milton Hinnant <miltonhinnant@me.com>
2023-02-20 01:59:47 -0500

    a4

--- shell/xsh_run.c
+++ shell/xsh_run.c
@@ -1,25 +1,27 @@
 #include <xinu.h>
 #include <shprototypes.h>
 #include <prodcons_bb.h>
+#define BUFFER_LENGTH 5
+
 
 void prodcons_bb(int nargs, char *args[])
 {
 	int producer_count, consumer_count, producer_iteration, consumer_iteration;  
 	char process_number[10];
 
-	producer_count = atoi(args[1]);
-	consumer_count = atoi(args[2]);
-	producer_iteration = atoi(args[3]);
-	consumer_iteration = atoi(args[4]);
+	producer_count = atoi(args[0]);
+	consumer_count = atoi(args[1]);
+	producer_iteration = atoi(args[2]);
+	consumer_iteration = atoi(args[3]);
 
+
+	producer_mutex = semcreate(BUFFER_LENGTH);
+	consumer_mutex = semcreate(0);
+	mutex = semcreate(1);
 
 	head = 0;
 	tail = 0;
 
-	producer_mutex = semcreate(0);
-	consumer_mutex = semcreate(5);
-	mutex = semcreate(1);
-	
 	if (nargs != 5)
     {
         printf("Syntax: run prodcons_bb <# producers> <# consumers> <# producer iterations> <# consumer iterations>\n");
@@ -33,13 +35,13 @@ void prodcons_bb(int nargs, char *args[])
 
 	for (int i = 0; i < producer_count; i++)
 	{
-		sprintf(process_number, "producer_%d", i);
-		resume(create((void *)producer_bb, 4096, 20, process_number, 1, producer_iteration));
+		sprintf(process_number, "producer_%d", i+1);
+		resume(create((void *)producer_bb, 1024, 20, process_number, 1, producer_iteration));
 	}
 	for (int i = 0; i < consumer_count; i++)
 	{
-		sprintf(process_number, "consumer_%d", i);
-		resume(create((void *)consumer_bb, 4096, 20, process_number, 1, consumer_iteration));
+		sprintf(process_number, "consumer_%d", i+1);
+		resume(create((void *)consumer_bb, 1024, 20, process_number, 1, consumer_iteration));
 	}
 }
 
@@ -71,5 +73,5 @@ shellcmd xsh_run(int nargs, char *args[]) {
 		printf("prodcons_bb\n");
 		return OK;
 	}
-	return 0;
+	return;
 }
